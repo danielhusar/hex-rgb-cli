2d792ccea75e75b2f657dc561f73ab1df82d9afc
Daniel Husar <dano.husar@gmail.com>
2015-08-25 17:43:40 +0100

    test

--- test.js
+++ test.js
@@ -1,13 +1,20 @@
 'use strict';
 var assert = require('assert');
-var childProcess = require('child_process');
+var spawn = require('child_process').spawn;
+var output;
+var cli;
 
 it('should print rgb from hex', function (cb) {
-	childProcess.execFile('./cli.js', ['#000'], {
-		cwd: __dirname
-	}, function (err, stdout) {
-		assert(!err, err);
-		assert(stdout.trim() === 'rgb(0, 0, 0)');
+	cli = spawn('./cli.js', ['#000']);
+
+	cli.stdout.on('data', function (data) {
+		output += data;
+	});
+
+	cli.on('close', function (code) {
+		assert(!code);
+		assert(output.trim() === 'rgb(0, 0, 0)');
 		cb();
 	});
 });
+
